979f489199f1ca6228eee247175705b343832e7f
Prajith P <prajith.palakkuda@cleartax.in>
2025-11-24 13:25:05 +0530

    fix: use skipper middleware

--- backend/routes/middleware/DisabledUnusedMethods.go
+++ backend/routes/middleware/DisabledUnusedMethods.go
@@ -8,11 +8,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-var disabledMethods = []string{"DELETE", "PUT", "PATCH", "POST"}
+var disabledMethods = []string{"DELETE", "PUT", "PATCH"}
 
 func DisableUnusedMethods(container container.Container) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
+			if shouldSkip(c) {
+				return next(c)
+			}
+
 			if slices.Contains(disabledMethods, c.Request().Method) {
 				return echo.ErrMethodNotAllowed
 			}
